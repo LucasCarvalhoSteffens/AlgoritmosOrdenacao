760cc65d40b4be5873af3601c21c54e055c3818d
Lucas de Carvalho Steffens <111320302+LucasCarvalhoSteffens@users.noreply.github.com>
2025-03-11 19:51:33 -0300

    Add FIleReader

---
+++ FileReader.py
@@ -0,0 +1,21 @@
+class FileReader:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_numbers(self):
+        try:
+            with open(self.filename, 'r') as file:
+                numbers = [int(line.strip()) for line in file.readlines()]
+            return numbers
+        except FileNotFoundError:
+            print("Arquivo não encontrado!")
+            return []
+        except ValueError:
+            print("Erro ao converter os valores para inteiros!")
+            return []
+
+# Exemplo de uso
+file_reader = FileReader("numeros_aleatorios.txt")
+desordenado = file_reader.read_numbers()
+if desordenado:
+    print(desordenado)
