72db55390774128ea1bb331df6e0b42e6082e6ea
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2025-03-17 11:24:18 -0300

    Update Insertion.py

--- Basicos/Insertion.py
+++ Basicos/Insertion.py
@@ -1,1 +1,11 @@
-from FileReader import FileReader
+class InsertionSort:
+    @staticmethod
+    def sort(arr):
+        n = len(arr)
+        for i in range(1, n):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and arr[j] > key:
+                arr[j + 1] = arr[j]  # Desloca os elementos para frente
+                j -= 1
+            arr[j + 1] = key  # Insere o elemento na posição correta
