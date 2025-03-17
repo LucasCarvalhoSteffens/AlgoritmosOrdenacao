0323415f51d12d13a0590942515baad3b9fd11f8
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2025-03-17 16:18:55 -0300

    Update ShellSort.py

--- OutrosSugeridos/ShellSort.py
+++ OutrosSugeridos/ShellSort.py
@@ -1,1 +1,18 @@
-from FileReader import FileReader
+class ShellSort:
+    @staticmethod
+    def sort(arr):
+        n = len(arr)
+        gap = n // 2  # Inicializa o intervalo
+        
+        while gap > 0:
+            for i in range(gap, n):
+                temp = arr[i]
+                j = i
+                while j >= gap and arr[j - gap] > temp:
+                    arr[j] = arr[j - gap]
+                    j -= gap
+                arr[j] = temp
+            
+            gap //= 2  # Reduz o intervalo
+        
+        return arr
