9504a61d81f238a610624c92cef1daec55f41c68
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2025-03-17 11:23:59 -0300

    Update BubbleMod.py

--- Basicos/BubbleMod.py
+++ Basicos/BubbleMod.py
@@ -1,1 +1,12 @@
-from FileReader import FileReader
+class BubbleSortMod:
+    @staticmethod
+    def sort(arr):
+        n = len(arr)
+        for i in range(n):
+            trocou = False  # Flag para verificar se houve troca
+            for j in range(n - 1 - i):  # Evita comparar elementos já ordenados
+                if arr[j] > arr[j + 1]:  # Compara elementos adjacentes
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]  # Troca os elementos
+                    trocou = True  # Marca que houve troca
+            if not trocou:
+                break  # Se não houve troca, a lista já está ordenada
