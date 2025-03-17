e751f024994981580ab5cd33f901b03b2ca643e4
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2025-03-17 11:28:08 -0300

    Update Quick.py

--- Avancados/Quick.py
+++ Avancados/Quick.py
@@ -1,1 +1,10 @@
-from FileReader import FileReader
+class QuickSort:
+    @staticmethod
+    def sort(arr):
+        if len(arr) <= 1:
+            return arr
+        pivot = arr[len(arr) // 2]  # Escolhe o pivô como o elemento do meio
+        left = [x for x in arr if x < pivot]  # Elementos menores que o pivô
+        middle = [x for x in arr if x == pivot]  # Elementos iguais ao pivô
+        right = [x for x in arr if x > pivot]  # Elementos maiores que o pivô
+        return QuickSort.sort(left) + middle + QuickSort.sort(right)
