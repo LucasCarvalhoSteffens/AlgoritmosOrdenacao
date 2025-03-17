8ce2b48f6f551666ddb42a97e7e27f32795c3131
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2025-03-17 11:24:36 -0300

    Update Selection.py

--- Basicos/Selection.py
+++ Basicos/Selection.py
@@ -1,1 +1,10 @@
-from FileReader import FileReader
+class SelectionSort:
+    @staticmethod
+    def sort(arr):
+        n = len(arr)
+        for i in range(n):
+            min_idx = i
+            for j in range(i + 1, n):
+                if arr[j] < arr[min_idx]:  # Encontra o menor elemento
+                    min_idx = j
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]  # Troca o menor elemento com o primeiro da parte não ordenada
